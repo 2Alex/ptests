219f7182241fad0e40253d6a5a27950ae8f75d5e
Alex <2alex.new@gmail.com>
2014-04-25 16:07:10 +0400

    Тест с переменным числом параметров и проверкой на неизменный результат, не нуждающийся в имени.

---
+++ ParameterizedTestSamples/Sample1/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ParameterizedTestSamples.Sample1
+{
+    public static class Calculator
+    {
+        /// <summary>
+        /// Возвращает true, если числа являются взаимно простыми.
+        /// </summary>
+        public static bool IsCoprimeNumbers(int n1, int n2)
+        {
+            int max = Math.Max(n1, n2);
+            for (int i = 2; i <= max; i++)
+            {
+                if (n1 % i == 0
+                 && n2 % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ ParameterizedTestSamples/Sample1/TestFixture.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ParameterizedTestSamples.Sample1
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [TestCase(14, 25)]
+        public void IsCoprimeNumbersTest(int n1, int n2)
+        {
+            Assert.That(Calculator.IsCoprimeNumbers(n1, n2), Is.True);
+        }
+    }
+}
