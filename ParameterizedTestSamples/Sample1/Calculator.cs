113357fdf663d48a8763c488717c3c2a27db1972
Alex <2alex.new@gmail.com>
2014-04-25 16:17:34 +0400

    Расщепление одного теста на два: для позитивных и негативных случаев. Модификация алгоритма.

--- ParameterizedTestSamples/Sample1/Calculator.cs
+++ ParameterizedTestSamples/Sample1/Calculator.cs
@@ -9,6 +9,15 @@ namespace ParameterizedTestSamples.Sample1
         /// </summary>
         public static bool IsCoprimeNumbers(int n1, int n2)
         {
+            if (n1 == n2)
+            {
+                return false;
+            }
+            if (n1 < 2 || n2 < 2)
+            {
+                return false;
+            }
+
             int max = Math.Max(n1, n2);
             for (int i = 2; i <= max; i++)
             {
--- ParameterizedTestSamples/Sample1/TestFixture.cs
+++ ParameterizedTestSamples/Sample1/TestFixture.cs
@@ -5,10 +5,23 @@ namespace ParameterizedTestSamples.Sample1
     [TestFixture]
     public class CalculatorTest
     {
+        [TestCase(2, 3)]
         [TestCase(14, 25)]
-        public void IsCoprimeNumbersTest(int n1, int n2)
+        [TestCase(3, 5)]
+        [TestCase(8, 3)]
+        public void IsCoprimeNumbersPositiveTest(int n1, int n2)
         {
-            Assert.That(Calculator.IsCoprimeNumbers(n1, n2), Is.True);
+            Assert.IsTrue(Calculator.IsCoprimeNumbers(n1, n2));
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(1, 0)]
+        [TestCase(2, 2)]        
+        [TestCase(2, 4)]        
+        public void IsCoprimeNumbersNegativeTest(int n1, int n2)
+        {
+            Assert.IsFalse(Calculator.IsCoprimeNumbers(n1, n2));
         }
     }
 }
